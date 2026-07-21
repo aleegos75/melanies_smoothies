6a8da07c13386e1578772497e815303a8a26d320
aleegos75 <alejandro.flores@egosbi.com>
2026-07-21 14:35:23 -0600

    Update streamit_app.py
    Se movio el frame de datos a el for de fruit_choosen

--- streamit_app.py
+++ streamit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 ##from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 
 
@@ -36,6 +37,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -52,8 +55,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-import requests
+
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-##st.text(smoothiefroot_response.json())
 st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
